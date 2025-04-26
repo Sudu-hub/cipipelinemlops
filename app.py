797cb5d384997c45d5dcde6f7c5352f7cd2c118e
sudu-hub <sudarshansahane1044@gmail.com>
2025-04-26 15:45:46 +0530

    again initial commit

--- app.py
+++ app.py
@@ -2,16 +2,13 @@ def add(a, b):
     return a + b
 
 
-def sub(a, b):
-    return a-b
 
 
-def mul(a, b):
-    return a*b
 
 
-if __name__ == "__main__":
-    result1 = add(4, 5)
-    result2 = sub(4, 1)
+def sub(a,b): return a+b
 
-    print(result1, result2)
+result1 = add(4,5)
+result2 = sub(4,1)
+
+print(result1, result2)
